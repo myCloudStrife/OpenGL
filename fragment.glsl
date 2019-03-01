6e841a212824d09b74e6fda3f476b131260d38ee
Alex <Alex@AlexComp>
2019-03-01 21:01:53 +0200

    refraction

--- fragment.glsl
+++ fragment.glsl
@@ -1,21 +1,20 @@
 #version 330
 
-#define float2 vec2
-#define float3 vec3
-#define float4 vec4
-#define float4x4 mat4
-#define float3x3 mat3
-
 const float EPSILON = 0.0001;
-const float MAX_DISTANCE = 50.0;
+const float MAX_DISTANCE = 60.0;
 const int MAX_STEP = 1024;
-const int MAX_REFLECTION = 10;
+const int MAX_REFLECTION = 4;
+const int MAX_REFRACTION = 4;
 const int LIGHTS_NUM = 2;
 
 struct Light {
     vec3 pos;
     vec3 intensity;
 };
+
+Light lights[2] = Light[2](
+        Light(vec3(2.75, 2, 0), vec3(1, 1, 1)),
+        Light(vec3(-2.75, 2, 0), vec3(1, 1, 1)));
 
 in vec2 fragmentTexCoord;
 
@@ -37,6 +36,16 @@ vec3 EyeRayDir(vec2 coord, vec2 size) {
     return normalize(ray_dir);
 }
 
+vec3 rotateX(vec3 p, float phi) {
+    return vec3(p.x, p.y * cos(phi) - p.z * sin(phi), p.z * cos(phi) + p.y * sin(phi));
+}
+
+float sdTriPrism( vec3 p, vec2 h )
+{
+    vec3 q = abs(p);
+    return max(q.z-h.y,max(q.x*0.866025+p.y*0.5,-p.y)-h.x*0.5);
+}
+
 float distSphere(vec3 p, float r) {
     return length(p) - r;
 }
@@ -48,8 +57,7 @@ float distRoundBox(vec3 p, vec3 b, float r) {
 float length8(vec2 p) {
     vec2 pp = p * p;
     vec2 p4 = pp * pp;
-    vec2 p8 = p4 * p4;
-    return pow(p8.x + p8.y, 0.125);
+    return pow(dot(p4, p4), 0.125);
 }
 
 float distTorus82(vec3 p, vec2 t) {
@@ -68,8 +76,7 @@ float distHexPrism2(vec3 p, vec2 h) {
 }
 
 float distTerrain(vec3 p) {
-    float dmin = 1e38;
-    dmin = min(dmin, distHexPrism2((p - vec3(0, -4, 1)).xzy, vec2(1, 2)));
+    float dmin = distHexPrism2((p - vec3(0, -4, 1)).xzy, vec2(1, 2));
     dmin = min(dmin, distHexPrism2((p - vec3(0, -3.7, -1)).xzy, vec2(1, 2)));
     dmin = min(dmin, distHexPrism2((p - vec3(2, -3.85, 0)).xzy, vec2(1, 2)));
     dmin = min(dmin, distHexPrism2((p - vec3(2, -3.5, 2)).xzy, vec2(1, 2)));
@@ -81,8 +88,11 @@ float distMirror(vec3 p) {
             //distRoundBox(p - vec3(2, 5, -1), vec3(2, 0.5, 2), 0.05));
 }
 
-vec3 rotateX(vec3 p, float phi) {
-    return vec3(p.x, p.y * cos(phi) - p.z * sin(phi), p.z * cos(phi) + p.y * sin(phi));
+float distLens(vec3 p) {
+    //return distRoundBox(p - vec3(0, 7, -2), vec3(2, 2, 2), 0.01);
+    return abs(max(distSphere(p - vec3(0, 9, 5), 5.5),
+            distRoundBox(p - vec3(0, 9, -1), vec3(3, 3, 1), 0.01)));
+            //distSphere(p - vec3(0, 7, -1.5), 2)));
 }
 
 float distRings(vec3 p) {
@@ -92,15 +102,22 @@ float distRings(vec3 p) {
 
 float distScene(vec3 p) {
     float dmin = 1e38;
-    //dmin = min(dmin, distSphere(p - vec3(0, 0, -1), 1));
+    dmin = min(dmin, sdTriPrism(p - vec3(0, 9, 20), vec2(1, 1)));
+    //dmin = min(dmin, sdTriPrism(p - vec3(0, 7, -20), vec2(1, 1)));
     dmin = min(dmin, distRings(p));
-    //dmin = min(dmin, distTorus82(p - vec3(0, 10, 0), vec2(3, 0.15)));
+    dmin = min(dmin, distTorus82((p - vec3(0, 9, 0)).xzy, vec2(2.45, 0.15)));
     //dmin = min(dmin, distRoundBox(p - vec3(5, 0, 0), vec3(1, 1, 2), 0.05));
     dmin = min(dmin, distMirror(p));
-    dmin = min(dmin, distTerrain(p));
+    dmin = min(dmin, distLens(p));
+    //dmin = min(dmin, distTerrain(p));
     return dmin;
 }
 
+vec3 myrefract(vec3 v, vec3 n, float k) {
+    float nv = dot(n, v);
+    float knormal = (sqrt((k * k - 1.0) / (nv * nv) + 1.0) - 1.0) * nv;
+    return normalize(v + knormal * n);
+}
 
 float getShortDistance(vec3 src, vec3 dir, float start) {
     float dist, fulldist = start;
@@ -118,6 +135,13 @@ float getShortDistance(vec3 src, vec3 dir, float start) {
     return fulldist;
 }
 
+vec3 lensNormal(vec3 p) {
+    if (p.z > -EPSILON) {
+        return vec3(0, 0, 1);
+    } else {
+        return (p - vec3(0, 9, 5)) / 5.5;
+    }
+}
 
 vec3 estimateNormal(vec3 z) {
     vec3 z1 = z + vec3(EPSILON, 0, 0);
@@ -132,24 +156,17 @@ vec3 estimateNormal(vec3 z) {
     return normalize(vec3(dx, dy, dz) / (2.0 * EPSILON));
 }
 
-vec3 illumPhong(vec3 kd, vec3 ks, float shininess, vec3 p,
+vec3 illumPhong(vec3 kd, vec3 ks, float shininess, vec3 p, vec3 n,
         vec3 light_pos, vec3 intensity, vec3 eye_dir) {
-    vec3 n = estimateNormal(p);
     vec3 l = normalize(light_pos - p);
     vec3 r = normalize(reflect(-l, n));
-    vec3 color = kd * max(dot(l, n), 0) * intensity;
-    color += ks * pow(max(dot(r, -eye_dir), 0.0), shininess) * intensity;
-    return color;
+    vec3 color = kd * max(dot(l, n), 0);
+    color += ks * pow(max(dot(r, -eye_dir), 0.0), shininess);
+    return color * intensity;
 }
 
-vec3 illumination(vec3 p, vec3 eye_dir) {
-    
-    Light lights[LIGHTS_NUM];
-    lights[0].pos = vec3(2.75, 2, 0);
-    lights[0].intensity = vec3(1, 1, 1);
-    lights[1].pos = vec3(-2.75, 2, 0);
-    lights[1].intensity = vec3(1, 1, 1);
-    
+vec3 illumination(vec3 p, vec3 eye_dir, vec3 n) {
+
     mat3 k;
     float shininess;
     if (distTerrain(p) < EPSILON * 2) {
@@ -185,19 +202,21 @@ vec3 illumination(vec3 p, vec3 eye_dir) {
             );
             shininess = 30;
         }
-    } else if (distMirror(p) < EPSILON * 2) {
+    } else if (distMirror(p) < EPSILON * 2 || distLens(p) < EPSILON * 2) {
         k = mat3(
                 0, 0, 0,
                 0, 0, 0,
                 0.7, 0.7, 0.7
         );
         shininess = 30;
-    } else {
+    } else if (distRings(p) < EPSILON * 2){
         k = mat3(
-                0, 0.1, 0,
-                0.1, 0.35, 0.1,
-                0.45, 0.55, 0.45
+                0.19225, 0.19225, 0.19225,
+                0.50754, 0.50754, 0.50754,
+                0.508273, 0.508273, 0.508273
         );
+        shininess = 40;
+    } else {
         k = mat3(
                 0, 0.05, 0.05,
                 0.4, 0.5, 0.5,
@@ -206,10 +225,12 @@ vec3 illumination(vec3 p, vec3 eye_dir) {
         shininess = 25;
     }
     vec3 color = k[0];
+    //!!!!p += n * EPSILON * 2;
     for (int i = 0; i < LIGHTS_NUM; ++i) {
-        float d = getShortDistance(lights[i].pos, normalize(p - lights[i].pos), EPSILON * 2);
+        float d = getShortDistance(lights[i].pos,
+                normalize(p + n * EPSILON * 2 - lights[i].pos), EPSILON * 2);
         if (d > length(p - lights[i].pos) - EPSILON * 5) {
-            color += illumPhong(k[1], k[2], shininess, p,
+            color += illumPhong(k[1], k[2], shininess, p, n,
                     lights[i].pos, lights[i].intensity, eye_dir);
         }
     }
@@ -217,22 +238,38 @@ vec3 illumination(vec3 p, vec3 eye_dir) {
 }
 
 vec3 get_color(vec3 p, vec3 ray_dir) {
-    int refl = 0;
+    int ref = 0;
     float k = 1;
     vec3 color = vec3(0, 0, 0);
-    while (distMirror(p) < EPSILON * 2 && refl < MAX_REFLECTION) {
-        color += illumination(p, ray_dir);
-        vec3 n = estimateNormal(p);
+    vec3 n = estimateNormal(p);
+    while (distMirror(p) < EPSILON * 2 && ref < MAX_REFLECTION) {
+        color += illumination(p, ray_dir, n);
         ray_dir = reflect(ray_dir, n);
         float dist = getShortDistance(p, ray_dir, EPSILON * 2);
         k *= 0.8;
         if (dist > MAX_DISTANCE - EPSILON) {
             return color + mix(texture(skybox, -ray_dir).xyz, vec3(0.5, 0.5, 0.5), 1 - k);
         }
-        refl++;
+        ref++;
         p += ray_dir * dist;
+        n = estimateNormal(p);
     }
-    return color + mix(illumination(p, ray_dir), vec3(0.5, 0.5, 0.5), 1 - k);
+    ref = 0;
+    while (distLens(p) < EPSILON * 2 && ref < MAX_REFRACTION) {
+        n = lensNormal(p);
+        //n = estimateNormal(p);
+        color += illumination(p, ray_dir, n);
+        ray_dir = myrefract(ray_dir, n, 1.52);
+        float dist = getShortDistance(p, ray_dir, EPSILON * 2);
+        k *= 0.8;
+        if (dist > MAX_DISTANCE - EPSILON) {
+            return color + mix(texture(skybox, -ray_dir).xyz, vec3(0.5, 0.5, 0.5), 1 - k);
+        }
+        ref++;
+        p += ray_dir * dist;
+        n = estimateNormal(p);
+    }
+    return color + mix(illumination(p, ray_dir, n), vec3(0.5, 0.5, 0.5), 1 - k);
 }
 
 
@@ -243,11 +280,9 @@ void main(void) {
     
     vec3 ray_pos = g_position; 
     vec3 ray_dir = EyeRayDir(coord, screenSize);
-    ray_dir = float3x3(g_rotate) * ray_dir;
+    ray_dir = mat3(g_rotate) * ray_dir;
     
     float dist = getShortDistance(ray_pos, ray_dir, 0);
-    
-    
     
     if (dist < MAX_DISTANCE - EPSILON) {
         ray_pos += ray_dir * dist;
